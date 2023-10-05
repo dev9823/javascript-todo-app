dff90f071a9590c0f00188bacc07d9d2114374ed
Eyob Abdella <eyobabdellasharo@gmail.com>
2023-10-05 08:30:34 -0500

    Store tasks in local storage

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 let taskList = document.getElementById("taskList");
 
+function createTaskElement({ text, completed }) {
+  let task = document.createElement("li");
+  let textNode = document.createElement("p");
+  let span = document.createElement("span");
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = completed;
+  textNode.textContent = text;
+  span.innerHTML = "&#10006;";
+
+  task.appendChild(checkbox);
+  task.appendChild(textNode);
+  task.appendChild(span);
+
+  return task;
+}
+
 document
   .getElementById("addTaskForm")
   .addEventListener("submit", function (event) {
@@ -8,19 +25,10 @@ document
 
     if (taskInput.value === "") alert("You have to write something!");
     else {
-      let task = document.createElement("li");
-      let text = document.createElement("p");
-      let span = document.createElement("span");
-      let checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      text.textContent = taskInput.value;
-      span.innerHTML = "&#10006;";
-
-      task.appendChild(checkbox);
-      task.appendChild(text);
-      task.appendChild(span);
+      let task = createTaskElement({ text: taskInput.value, completed: false });
       taskList.appendChild(task);
       taskInput.value = "";
+      saveTask();
     }
   });
 
@@ -31,6 +39,7 @@ taskList.addEventListener("click", function (event) {
     );
     checkbox.checked = !checkbox.checked;
     event.target.classList.toggle("checked");
+    saveTask();
   } else if (
     event.target.tagName === "INPUT" &&
     event.target.type === "checkbox"
@@ -38,6 +47,29 @@ taskList.addEventListener("click", function (event) {
     const checkbox = event.target;
     const text = checkbox.nextElementSibling;
     text.classList.toggle("checked");
-  } else if (event.target.tagName === "SPAN")
+    saveTask();
+  } else if (event.target.tagName === "SPAN") {
     event.target.parentElement.remove();
+    saveTask();
+  }
 });
+
+function saveTask() {
+  const tasks = Array.from(taskList.children).map((li) => {
+    const text = li.querySelector("p").textContent;
+    const completed = li.querySelector('input[type="checkbox"]').checked;
+    return { text, completed };
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function showTask() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  taskList.innerHTML = "";
+
+  for (const task of tasks) {
+    let taskElement = createTaskElement(task);
+    taskList.appendChild(taskElement);
+  }
+}
+
+showTask();
